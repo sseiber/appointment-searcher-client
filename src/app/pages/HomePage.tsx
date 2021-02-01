fc577dfe4afba48548bc472747c8b2a1bae29e18
Scott Seiber <scott.seiber@outlook.com>
2021-02-01 10:01:48 -0800

    Navigate from HomePage

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Grid, Message } from 'semantic-ui-react';
+import { Grid, Message, Button } from 'semantic-ui-react';
 
 export class HomePage extends React.Component<any, any> {
     public render() {
@@ -11,6 +11,7 @@ export class HomePage extends React.Component<any, any> {
                             <h2>Appointment Searcher Client</h2>
                             <p>Welcome</p>
                         </Message>
+                        <Button size={'tiny'} color={'blue'} href="/search">Get Started</Button>
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
